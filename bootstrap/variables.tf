18e3d8c9768493c3d5c3a44c79c03e96769f52a2
Shailendra Singh <shailendra4dev@gmail.com>
2026-07-31 12:33:27 +0530

    Updated and added bootstrap for github actions.

---
+++ bootstrap/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+
+  default = "ap-south-1"
+
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,3 +3,22 @@ provider "aws" {
   region = var.aws_region
 
 }
+
+terraform {
+
+  required_version = ">=1.6"
+
+  backend "s3" {
+
+    bucket = "funschoolapi-terraform-state-846511227413"
+
+    key = "dev/terraform.tfstate"
+
+    region = "ap-south-1"
+
+    dynamodb_table = "terraform-lock"
+
+    encrypt = true
+
+  }
+}
