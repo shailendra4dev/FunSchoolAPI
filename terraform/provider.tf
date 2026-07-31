9d4d1acde9076a3800985f0bef73ab29492f7244
Shailendra Singh <shailendra4dev@gmail.com>
2026-07-31 13:13:51 +0530

    updated

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,20 +4,6 @@ terraform {
 
   backend "s3" {}
 
-  required_providers {
-
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.7"
-    }
-
-  }
-
 }
 
 provider "aws" {
