0301e22cbd197eff88cd1c7aeb3b8867e080f5cf
Shailendra Singh <shailendra4dev@gmail.com>
2026-08-06 13:46:40 +0530

    Added cors.

--- src/utils/response.js
+++ src/utils/response.js
@@ -5,8 +5,10 @@ export const response = (
   statusCode,
 
   headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Headers": "Content-Type,Authorization",
+    "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS"
   },
 
   body: JSON.stringify(data),
--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -82,6 +82,170 @@ locals {
 
 }
 
+resource "aws_api_gateway_method" "posts_options" {
+
+  rest_api_id = aws_api_gateway_rest_api.post_api.id
+
+  resource_id = aws_api_gateway_resource.posts.id
+
+  http_method = "OPTIONS"
+
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "posts_options" {
+
+  rest_api_id = aws_api_gateway_rest_api.post_api.id
+
+  resource_id = aws_api_gateway_resource.posts.id
+
+  http_method = aws_api_gateway_method.posts_options.http_method
+
+  type = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "posts_options" {
+
+  rest_api_id = aws_api_gateway_rest_api.post_api.id
+
+  resource_id = aws_api_gateway_resource.posts.id
+
+  http_method = aws_api_gateway_method.posts_options.http_method
+
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+
+    "method.response.header.Access-Control-Allow-Origin"  = true
+
+    "method.response.header.Access-Control-Allow-Headers" = true
+
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "posts_options" {
+
+  rest_api_id = aws_api_gateway_rest_api.post_api.id
+
+  resource_id = aws_api_gateway_resource.posts.id
+
+  http_method = aws_api_gateway_method.posts_options.http_method
+
+  status_code = aws_api_gateway_method_response.posts_options.status_code
+
+  response_parameters = {
+
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS'"
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.posts_options
+  ]
+}
+
+
+
+
+
+
+
+
+
+resource "aws_api_gateway_method" "post_id_options" {
+
+  rest_api_id = aws_api_gateway_rest_api.post_api.id
+
+  resource_id = aws_api_gateway_resource.post_id.id
+
+  http_method = "OPTIONS"
+
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "post_id_options" {
+
+  rest_api_id = aws_api_gateway_rest_api.post_api.id
+
+  resource_id = aws_api_gateway_resource.post_id.id
+
+  http_method = aws_api_gateway_method.posts_options.http_method
+
+  type = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "post_id_options" {
+
+  rest_api_id = aws_api_gateway_rest_api.post_api.id
+
+  resource_id = aws_api_gateway_resource.post_id.id
+
+  http_method = aws_api_gateway_method.posts_options.http_method
+
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+
+    "method.response.header.Access-Control-Allow-Origin"  = true
+
+    "method.response.header.Access-Control-Allow-Headers" = true
+
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "post_id_options" {
+
+  rest_api_id = aws_api_gateway_rest_api.post_api.id
+
+  resource_id = aws_api_gateway_resource.post_id.id
+
+  http_method = aws_api_gateway_method.posts_options.http_method
+
+  status_code = aws_api_gateway_method_response.posts_options.status_code
+
+  response_parameters = {
+
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS'"
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.posts_options
+  ]
+}
+
+
+
+
+
+
+
+
+
 resource "aws_api_gateway_method" "posts" {
 
   for_each = local.api_methods
@@ -135,13 +299,20 @@ resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.post_api.id
 
   triggers = {
-    redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.posts.id,
-      aws_api_gateway_resource.post_id.id,
-      aws_api_gateway_method.posts,
-      aws_api_gateway_integration.posts
-    ]))
-  }
+  redeployment = sha1(jsonencode([
+    aws_api_gateway_resource.posts.id,
+    aws_api_gateway_resource.post_id.id,
+
+    aws_api_gateway_method.posts,
+    aws_api_gateway_integration.posts,
+
+    aws_api_gateway_method.posts_options,
+    aws_api_gateway_integration.posts_options,
+
+    aws_api_gateway_method.post_id_options,
+    aws_api_gateway_integration.post_id_options
+  ]))
+}
 
   lifecycle {
     create_before_destroy = true
