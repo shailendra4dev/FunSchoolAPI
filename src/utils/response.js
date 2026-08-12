c724679e24314e774a1770f22de50bcd1dec4318
Shailendra Singh <shailendra4dev@gmail.com>
2026-08-12 15:36:54 +0530

    added cloudfront domain.

--- src/utils/response.js
+++ src/utils/response.js
@@ -6,7 +6,7 @@ export const response = (
 
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Origin": "http://localhost:3000, https://d396lh7c4lwt6i.cloudfront.net",
     "Access-Control-Allow-Headers": "Content-Type,Authorization",
     "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS"
   },
